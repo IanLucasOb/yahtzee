733e909d8af86d11583e9b54efdb035679edc6c6
Eduardo Aleixo <eduardo.aleixo@el.com.br>
2023-11-14 21:04:03 -0300

    implementa classe Dado

--- src/Dado.java
+++ src/Dado.java
@@ -1,3 +1,18 @@
+import java.util.Random;
+
 public class Dado {
+    private int valorFace;
+
+    // public Dado() {
+    //     this.rolar();
+    // } seria o certo fazer no construtor?
     
+    public void rolar() {
+        Random gerador = new Random();
+        this.valorFace = gerador.nextInt(6) + 1;
+    }
+
+    public int getValor() {
+        return this.valorFace;
+    }
 }
