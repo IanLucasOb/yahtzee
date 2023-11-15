bf20589d59ce4ddfbf7f6234aea146117c691883
Vinicius <viviraleixo2005@gmail.com>
2023-11-15 02:26:11 -0300

    Atualizei a classe menu. Falta fazer um for para pergutar a quantidade de nomes corretas.

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Menu {
     private int escolhaDoUsuario;
     private Scanner scanner = new Scanner(System.in);
+    private GerenciadorArquivos gerenciarArqui = new GerenciadorArquivos();
+    private int quantDeJogadores;
 
     // public Menu() {
     // this.scanner = new Scanner(System.in);
@@ -18,10 +20,18 @@ public class Menu {
                             + "| 2 - Ver histórico                                 |\n"
                             + "| 0 - Sair do jogo                                  |\n"
                             + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-            this.escolhaDoUsuario = scanner.nextInt();
+            this.escolhaDoUsuario = scanner.nextInt(); // O nextInt(): não consumia a linha do (Enter);
+            scanner.nextLine(); // Consumir a quebra de linha pendente;
             switch (escolhaDoUsuario) {
                 case 1:
+                    System.out.println("| =-=-=-=-=- Digite a quantidade de jogadores -=-=-=-=-= |");
+                    this.quantDeJogadores = scanner.nextInt(); // Variável para guardar a quantidade;
+
+                    System.out.println("| =-=-=-=-=- Digite seu nome de jogador -=-=-=-=-= |");
+                    String nomeDoJogador = scanner.nextLine(); // Variável para guardar nome; 
+                    this.gerenciarArqui.gravarJogador(nomeDoJogador);
                     break; // Aqui irá vir a função jogar;
+
                 case 2:
                     break; // Aqui irá vir a função ver histórico;
                 case 0:
@@ -37,5 +47,9 @@ public class Menu {
 
     public int getEscolhaDoUsuario() {
         return this.escolhaDoUsuario; // Retorna a escolha do usuario;
+    }
+
+    public int geQuantDeJogadores(){
+        return this.quantDeJogadores;
     }
 }
