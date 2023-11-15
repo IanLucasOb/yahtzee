2b1bd620d847111350a60bbece26ac9aa0ee59f9
Bowie <marketing@letscode.com.br>
2023-11-15 02:09:44 -0300

    Adicionando atributos e métodos da classe Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,3 +1,26 @@
 public class Jogador {
+    private String nome;
+    private int pontuacaoJogador[] = new int[16];
+
+    public Jogador(){
+        for(int i = 0; i < 16; i++){
+            pontuacaoJogador[i] = 0;
+        }
+    }
     
+    public void setNome(String nome){ // Atribui um nome ao jogador.
+        this.nome = nome;
+    }
+
+    public String getNome(){ // Pega o nome do jogador.
+        return this.nome;
+    }
+
+    public void setPontuacaoJogador(int indice, int valor){ // Atribui um valor ao indice indicado na lista de pontuação do jogador. 
+        this.pontuacaoJogador[indice] = valor;
+    }
+    
+    public int[] getPontuacaoJogador(){ // Pega a lista de pontuação do jogador.
+        return this.pontuacaoJogador;
+    }
 }
